e57d08f4b15a7a2da7286b7ad587c91e6ee96295
ryuichi1208 <ryucrosskey@gmail.com>
2021-12-21 23:07:29 +0900

    add metrics threash

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/ryuichi1208/mackerel-plugin-linux-tcp-memory
 
 go 1.15
 
-require github.com/mackerelio/go-mackerel-plugin v0.1.2
+require (
+	github.com/Songmu/goxz v0.7.0 // indirect
+	github.com/mackerelio/go-mackerel-plugin v0.1.2
+	github.com/tcnksm/ghr v0.14.0 // indirect
+)
--- lib/linux_tcp_mem.go
+++ lib/linux_tcp_mem.go
@@ -2,8 +2,8 @@ package linuxTcp
 
 import (
 	"bufio"
-	"fmt"
 	"os"
+	"os/exec"
 	"strconv"
 	"strings"
 
@@ -18,11 +18,53 @@ var graphdef map[string]mp.Graphs = map[string]mp.Graphs{
 			{Name: "tcp_memory_size", Label: "tcp_memory", Diff: false},
 		},
 	},
+	"linutTcp.tcpMemMin": {
+		Label: "Linux Tcp Memory",
+		Unit:  "integer",
+		Metrics: []mp.Metrics{
+			{Name: "tcp_memory_size_min", Label: "tcp_memory", Diff: false},
+		},
+	},
+	"linutTcp.tcpMemPressure": {
+		Label: "Linux Tcp Memory",
+		Unit:  "integer",
+		Metrics: []mp.Metrics{
+			{Name: "tcp_memory_size_pressure", Label: "tcp_memory", Diff: false},
+		},
+	},
+	"linutTcp.tcpMemMax": {
+		Label: "Linux Tcp Memory",
+		Unit:  "integer",
+		Metrics: []mp.Metrics{
+			{Name: "tcp_memory_size_max", Label: "tcp_memory", Diff: false},
+		},
+	},
 	"linutTcp.udpMem": {
 		Label: "Linux udp Memory",
 		Unit:  "integer",
 		Metrics: []mp.Metrics{
 			{Name: "udp_memory_size", Label: "udp_memory", Diff: false},
+		},
+	},
+	"linutTcp.udpMemMin": {
+		Label: "Linux udp Memory",
+		Unit:  "integer",
+		Metrics: []mp.Metrics{
+			{Name: "udp_memory_size_min", Label: "udp_memory", Diff: false},
+		},
+	},
+	"linutTcp.udpPressure": {
+		Label: "Linux udp Memory",
+		Unit:  "integer",
+		Metrics: []mp.Metrics{
+			{Name: "udp_memory_size_pressure", Label: "udp_memory", Diff: false},
+		},
+	},
+	"linutTcp.udpMemMax": {
+		Label: "Linux udp Memory",
+		Unit:  "integer",
+		Metrics: []mp.Metrics{
+			{Name: "udp_memory_size_max", Label: "udp_memory", Diff: false},
 		},
 	},
 }
@@ -34,13 +76,41 @@ type LinuxTcpMemPlugin struct {
 var pagesize int = os.Getpagesize()
 
 type ProtoMemPages struct {
-	tcpMem int
-	udpMem int
+	tcpMem         int
+	tcpMemMin      int
+	tcpMemPressure int
+	tcpMemMax      int
+	udpMem         int
+	udpMemMin      int
+	udpMemPressure int
+	udpMemMax      int
+}
+
+func (pmp *ProtoMemPages) parseProtobufMemorytTreshhold() {
+	var arr []string
+	for _, param := range []string{"net.ipv4.tcp_mem", "net.ipv4.udp_mem"} {
+		res, err := exec.Command("sysctl", param).Output()
+		arr = strings.Split(string(res), "\t")
+		if err != nil {
+			panic(err)
+		}
+		switch param {
+		case "net.ipv4.tcp_mem":
+			pmp.tcpMemMin, _ = strconv.Atoi(strings.Split(arr[0], " ")[2])
+			pmp.tcpMemPressure, _ = strconv.Atoi(arr[1])
+			pmp.tcpMemMax, _ = strconv.Atoi(strings.TrimRight(arr[2], "\n"))
+		case "net.ipv4.udp_mem":
+			pmp.udpMemMin, _ = strconv.Atoi(strings.Split(arr[0], " ")[2])
+			pmp.udpMemPressure, _ = strconv.Atoi(arr[1])
+			pmp.udpMemMax, _ = strconv.Atoi(strings.TrimRight(arr[2], "\n"))
+		}
+	}
 }
 
 func parseSockstatMem() *ProtoMemPages {
 	var txt string
 	pmp := new(ProtoMemPages)
+	pmp.parseProtobufMemorytTreshhold()
 
 	fp, err := os.Open("/proc/net/sockstat")
 	if err != nil {
@@ -64,10 +134,15 @@ func parseSockstatMem() *ProtoMemPages {
 
 func (ltmp LinuxTcpMemPlugin) FetchMetrics() (map[string]float64, error) {
 	pmp := parseSockstatMem()
-	fmt.Println(pmp)
 	m := map[string]float64{
-		"tcp_memory_size": float64(pmp.tcpMem * pagesize),
-		"udp_memory_size": float64(pmp.udpMem * pagesize),
+		"tcp_memory_size":          float64(pmp.tcpMem * pagesize),
+		"tcp_memory_size_min":      float64(pmp.tcpMemMin * pagesize),
+		"tcp_memory_size_pressure": float64(pmp.tcpMemPressure * pagesize),
+		"tcp_memory_size_max":      float64(pmp.tcpMemMax * pagesize),
+		"udp_memory_size":          float64(pmp.udpMem * pagesize),
+		"udp_memory_size_min":      float64(pmp.udpMemMin * pagesize),
+		"udp_memory_size_pressure": float64(pmp.udpMemPressure * pagesize),
+		"udp_memory_size_max":      float64(pmp.udpMemMax * pagesize),
 	}
 	return m, nil
 }
